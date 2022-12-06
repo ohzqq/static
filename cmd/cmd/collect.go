2f678d7f08fddb483d73ace53cbfd0775a41871e
ohzqq <iamchurkey@gmail.com>
2022-12-06 15:39:58 -0500

    recurse

--- cmd/cmd/collect.go
+++ cmd/cmd/collect.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -13,13 +9,7 @@ import (
 // collectCmd represents the collect command
 var collectCmd = &cobra.Command{
 	Use:   "collect",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("collect called")
 	},
