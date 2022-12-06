c1b80faa8657aaeec5e6a9bba31bea742239476f
ohzqq <iamchurkey@gmail.com>
2022-12-06 09:45:09 -0500

    separate filetree css

--- static/js/filetree.js
+++ static/js/filetree.js
@@ -7,12 +7,3 @@ for (i = 0; i < toggler.length; i++) {
     this.classList.toggle("caret-down");
   });
 } 
-
-function w3_open_sidebar() {
-  document.getElementById("filetree").style.width = "100%";
-  document.getElementById("filetree").style.display = "block";
-}
-
-function w3_close_sidebar() {
-  document.getElementById("filetree").style.display = "none";
-}
